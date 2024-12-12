f8d192c94013e7502baf538c988f64e2e729e144
Dmitriy Usatov <usatov.dv@gmail.com>
2024-12-12 17:13:21 +0300

    version: 0.0.1+1

---
+++ tools/scripts/build_dev_ipa.sh
@@ -0,0 +1,5 @@
+flutter build ipa --dart-define-from-file=tools/configs/dev_config.json
+
+BUILD_VER=`grep ^"version: " pubspec.yaml | cut -b 10-`
+mkdir -p build_artifacts
+mv build/ios/ipa/Partnext.ipa build_artifacts/partnext-$BUILD_VER-dev.ipa
