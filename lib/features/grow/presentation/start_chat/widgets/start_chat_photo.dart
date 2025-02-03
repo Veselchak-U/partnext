a43255630415e46abb6bcb59395c48af9a6c8161
Dmitriy Usatov <usatov.dv@gmail.com>
2025-02-03 16:26:42 +0300

    Improved "start chat screen" layout

--- lib/features/grow/presentation/start_chat/widgets/start_chat_photo.dart
+++ lib/features/grow/presentation/start_chat/widgets/start_chat_photo.dart
@@ -5,27 +5,28 @@ import 'package:partnext/app/style/app_colors.dart';
 
 class StartChatPhoto extends StatelessWidget {
   final String imageUrl;
+  final double width;
+  final double height;
 
   const StartChatPhoto({
     required this.imageUrl,
+    required this.width,
+    required this.height,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: imageUrl,
-      child: Container(
-        width: 190.w,
-        height: 255.h,
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          borderRadius: BorderRadius.circular(10).r,
-          border: Border.all(color: AppColors.white, width: 3),
-          image: DecorationImage(
-            image: CachedNetworkImageProvider(imageUrl),
-            fit: BoxFit.cover,
-          ),
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(10).r,
+        border: Border.all(color: AppColors.white, width: 3),
+        image: DecorationImage(
+          image: CachedNetworkImageProvider(imageUrl),
+          fit: BoxFit.cover,
         ),
       ),
     );
--- lib/features/grow/presentation/start_chat/widgets/start_chat_photo_block.dart
+++ lib/features/grow/presentation/start_chat/widgets/start_chat_photo_block.dart
@@ -1,9 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:partnext/app/generated/assets.gen.dart';
-import 'package:partnext/features/grow/presentation/start_chat/widgets/start_chat_mini_photo.dart';
 import 'package:partnext/features/grow/presentation/start_chat/widgets/start_chat_photo.dart';
 
 class StartChatPhotoBlock extends StatelessWidget {
@@ -22,48 +22,47 @@ class StartChatPhotoBlock extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return KeyboardVisibilityBuilder(
-      builder: (context, isKeyboardVisible) {
-        return AnimatedSize(
-          duration: const Duration(milliseconds: 150),
-          child: isKeyboardVisible
-              ? StartChatMiniPhoto(
-                  imageUrl: partnerImageUrl,
-                  fullName: partnerFullName,
-                  position: partnerPosition,
-                )
-              : SizedBox(
-                  height: 392.h,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16).w,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          top: 0,
-                          right: 0,
-                          child: StartChatPhoto(imageUrl: myImageUrl),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          left: 0,
-                          child: StartChatPhoto(imageUrl: partnerImageUrl),
-                        ),
-                        Positioned(
-                          top: 35.h,
-                          left: 27.w,
-                          child: Image.asset(Assets.images.logoWhite.path, height: 88.h),
-                        ),
-                        Positioned(
-                          bottom: 19.h,
-                          right: 15.w,
-                          child: SvgPicture.asset(Assets.icons.approve.path, height: 92.h),
-                        ),
-                      ],
-                    ),
-                  ),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16).w,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final photoWidth = constraints.maxWidth * 0.6;
+          final photoHeight = min(255.h, constraints.maxHeight);
+
+          return Stack(
+            children: [
+              Positioned(
+                top: 35.h,
+                left: 27.w,
+                child: Image.asset(Assets.images.logoWhite.path, height: 88.h),
+              ),
+              Positioned(
+                bottom: 19.h,
+                right: 15.w,
+                child: SvgPicture.asset(Assets.icons.approve.path, height: 92.h),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: StartChatPhoto(
+                  imageUrl: myImageUrl,
+                  width: photoWidth,
+                  height: photoHeight,
                 ),
-        );
-      },
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: StartChatPhoto(
+                  imageUrl: partnerImageUrl,
+                  width: photoWidth,
+                  height: photoHeight,
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
