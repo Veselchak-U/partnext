6b7670bccc3f61812d143fb26d3ea131e7c577ce
Dmitriy Usatov <usatov.dv@gmail.com>
2025-04-04 19:45:29 +0300

    Fixed build bug after Xcode update

--- tools/scripts/build_dev_ipa.sh
+++ tools/scripts/build_dev_ipa.sh
@@ -3,3 +3,4 @@ flutter build ipa --dart-define-from-file=tools/configs/dev_config.json
 BUILD_VER=`grep ^"version: " pubspec.yaml | cut -b 10-`
 mkdir -p build_artifacts
 mv build/ios/ipa/Partnext.ipa build_artifacts/partnext-$BUILD_VER-dev.ipa
+zip -d build_artifacts/partnext-$BUILD_VER-dev.ipa ._Symbols/
--- tools/scripts/build_prod_ipa.sh
+++ tools/scripts/build_prod_ipa.sh
@@ -3,3 +3,4 @@ flutter build ipa --dart-define-from-file=tools/configs/prod_config.json
 BUILD_VER=`grep ^"version: " pubspec.yaml | cut -b 10-`
 mkdir -p build_artifacts
 mv build/ios/ipa/Partnext.ipa build_artifacts/partnext-$BUILD_VER-prod.ipa
+zip -d build_artifacts/partnext-$BUILD_VER-prod.ipa ._Symbols/
