6b8573bc8d8d1c034bf786a64c963cd156e65e75
Dmitriy Usatov <usatov.dv@gmail.com>
2025-04-14 11:49:22 +0300

    Added change locale button to profile screen

--- lib/common/buttons/change_locale_button.dart
+++ lib/common/buttons/change_locale_button.dart
@@ -4,21 +4,34 @@ import 'package:partnext/app/l10n/l10n.dart';
 import 'package:partnext/config.dart';
 
 class ChangeLocaleButton extends StatelessWidget {
-  const ChangeLocaleButton({super.key});
+  final bool isDebug;
+
+  const ChangeLocaleButton({
+    this.isDebug = true,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Config.isProdBuild
+    final buttonStyle = isDebug
+        ? ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll<Color?>(Colors.grey.withValues(alpha: 0.3)),
+            foregroundColor: const WidgetStatePropertyAll<Color?>(Colors.white),
+            fixedSize: WidgetStatePropertyAll<Size?>(Size.square(42.r)),
+          )
+        : ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll<Color?>(Colors.white),
+            foregroundColor: const WidgetStatePropertyAll<Color?>(Colors.black),
+            fixedSize: WidgetStatePropertyAll<Size?>(Size.square(42.r)),
+          );
+
+    return Config.isProdBuild && isDebug
         ? const SizedBox.shrink()
         : IconButton(
             onPressed: () => _changeLocale(context),
             icon: const Icon(Icons.translate),
             iconSize: 24.r,
-            style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll<Color?>(Colors.grey.withValues(alpha: 0.3)),
-              foregroundColor: const WidgetStatePropertyAll<Color?>(Colors.white),
-              fixedSize: WidgetStatePropertyAll<Size?>(Size.square(42.r)),
-            ),
+            style: buttonStyle,
           );
   }
 
--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:partnext/app/generated/assets.gen.dart';
 import 'package:partnext/app/l10n/l10n.dart';
+import 'package:partnext/common/buttons/change_locale_button.dart';
 import 'package:partnext/common/layouts/main_layout.dart';
 import 'package:partnext/common/widgets/loading_container_indicator.dart';
 import 'package:partnext/features/profile/presentation/profile_screen_vm.dart';
@@ -20,16 +21,17 @@ class ProfileScreen extends StatelessWidget {
     return MainLayout(
       titleText: context.l10n.profile,
       actions: [
+        IconButton(
+          padding: EdgeInsets.all(12).h,
+          icon: SvgPicture.asset(
+            Assets.icons.camera.path,
+            width: 24.h,
+          ),
+          onPressed: vm.changeUserAvatar,
+        ),
         Padding(
           padding: EdgeInsetsDirectional.only(end: 8.w),
-          child: IconButton(
-            padding: EdgeInsets.all(12).h,
-            icon: SvgPicture.asset(
-              Assets.icons.camera.path,
-              width: 24.h,
-            ),
-            onPressed: vm.changeUserAvatar,
-          ),
+          child: const ChangeLocaleButton(isDebug: false),
         ),
       ],
       extendBodyBehindAppBar: false,
